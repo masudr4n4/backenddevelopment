4f26ba2b418e5d2afd632fc337b3c7fb6a46cefe
masudrana <mdrana01726@gmail.com>
2025-04-17 16:09:24 +0600

    update tictoc

--- frontend/todo_app/src/componants/tictoctoe.jsx
+++ frontend/todo_app/src/componants/tictoctoe.jsx
@@ -1,25 +1,73 @@
 import { useState } from "react"
 
-function Player({name,symbol}){
-    const [playerName,updatePlayerName] = useState(name);
+const board = [
+    [null,null,null],
+    [null,null,null],
+    [null,null,null]
+]
+
+function Player({name,symbol,isActive}){
+    const [playerName,updatePlayerName] = useState(false);
     const [isEditing,updateEdit] = useState(false);
 
     function changeHandler(event){
         console.log(event);
         updatePlayerName(event.target.value)
     }
-    let playerBlock = <span className="text-2xl mr-5 flex-1 bg-cyan-500 w-32 p-2 shadow-amber-200 rounded-xl">{playerName}</span>
+    let playerBlock = <span className="text-2xl mr-5 flex-1 bg-cyan-200 w-32 p-2 shadow-amber-200 rounded-xl">{playerName}</span>
     if (isEditing){
         playerBlock= <input type="text" placeholder="Enter Name" className="input flex-1" required defaultValue={playerName} onChange={changeHandler}/>
     }
-    return <p className="flex-1 ml-20" playerid={symbol}>
+    return <div className="flex-1 text-center" playerid={symbol}>
+        <div>
         {playerBlock}
         <button className={`btn flex-1 ${isEditing?"bg-emerald-50":"bg-blue-300"}`} onClick={()=>updateEdit(!isEditing)}>{isEditing ? "Save" : "Edit"}</button>
-        <span className="ml-4 flex-1">Player Symbol:{symbol}</span>
-        </p>
+        </div>
+        <p className="flex-1">Player Symbol: <span className="text-2xl">{symbol}</span></p>
+        </div>
+}
+
+function Board(){
+    const [currentPlayer,updatePlayer] = useState('X');
+    const [currentBoard,updateBoard] = useState(board);
+    console.log("Printing the currentboard : ",currentBoard);
+    
+
+    function boardclickHandler(row,col){
+        let player = "X"
+        if (currentPlayer){
+            player = "O"
+        }
+        console.log("clicked element index: ",row,col)
+        updateBoard((currentBoard) =>{
+            let updatedboard = [...currentBoard.map((data)=>([...data]))]
+            console.log("Copied board: ",updatedboard);
+            if(!updatedboard[row][col]){
+                updatedboard[row][col] = player
+            }
+            return updatedboard;
+        })
+        updatePlayer(!currentPlayer)
+    }
+
+    return <>
+    {currentBoard.map((row,rowIndex)=>(
+        <ol key={rowIndex}>
+            <div className="flex flex-row" key={rowIndex}>
+                {row.map((col,colIndex)=>(
+                    <li key={colIndex} className="btn bg-amber-50 m-2 w-40 h-40" onClick={()=>(boardclickHandler(rowIndex,colIndex))}> 
+                    <button>
+                        {col}
+                    </button>
+                    </li>))}
+           </div>
+        
+        </ol>))}
+    </>
 }
 
 export default function TicTocToe(){
+    const [activePlayer,updateActivePlayer] = useState("X");
     return <>
     <div className="bg-cyan-100 mx-10 p-3">
         <h1 className="text-3xl font-bold text-center">
@@ -30,17 +78,8 @@ export default function TicTocToe(){
                <Player name={"player 1"} symbol={"X"}/>
                <Player name={"player 2"} symbol={"O"}/>
             </div>
-
-            <div className="board">
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
+            <div className="board flex flex-col justify-center items-center">
+                <Board />
             </div>
             
         </div>
