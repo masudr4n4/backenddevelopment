aa415df1fddfb552bf045a3e8e988dfd67956d2e
masudrana <mdrana01726@gmail.com>
2025-04-17 09:48:52 +0600

    update info baord

--- frontend/todo_app/src/componants/InfoBoard.jsx
+++ frontend/todo_app/src/componants/InfoBoard.jsx
@@ -1,18 +1,49 @@
-function Button({item,key}){
+// import data from "../data.js"
+
+import { useState } from "react"
+
+const data= {
+    "Python":{
+        'description':" PYTHON lorem ip helo world python lasd vmaloweifj slnlvaoie fn ike vnlavalsdfie nlav lwenviaoaonlkdnv loevna"
+    },
+    "JavaScript":{
+        'description':" JAVASCRIPT lorem ip helo worlorem ip helo world python lasd vmaloweifj slnlvaoie fn ike vnlavalsdfie nlav lwenhon"
+    },
+    "Java":{
+        'description':" JAVA lorem ip helo worem ip helo world python lasd vmaloweifj slnlvaoie fn ike vnlavalsdfie nlav lwenn"
+    },
+    "SQL":{
+        'description':" SQL lorem ip helo worem ip helo world python lasd vmaloweifj slnlvaoie fn ike vnlavalsdfie nlav lwenn"
+    },
+    "QA":{
+        'description':"QA orem ip helo worem ip helo world python lasd vmaloweifj slnlvaoie fn ike vnlavalsdfie nlav lwenn"
+    }
+}
+
+
+
+function Button({item,id,clickHandler,isSelected}){
 return (
-    <button className="btn" id={key}>{item}</button>
+    <button className={`btn flex-1 ${isSelected ? "btn-active":""}`} key={id} onClick={clickHandler}>{item}</button>
 )
 }
 
 export default function InfoBoard(){
-    const skills = ["Python","JavaScript","Java","SQL"]
+    const [selectedSKill,updateSkill] = useState("Python");
+    const skills = ["Python","JavaScript","Java","SQL","QA"]
     return <>
-    <div className="info-container">
-        <div className="info-button">
-        {skills.map((item,index)=>(<Button item={item} key={index}/>))}
+    <div className="info-container mx-12">
+        <div className="info-button flex ">
+        {skills.map((item,index)=>(<Button item={item} key={index} clickHandler={()=>updateSkill(item)} isSelected={item==selectedSKill}/>))}
         </div>
-        <div className="info"></div>
-    </div>
+        <div className="info">
+             <div className="card bg-base-100 shadow-lg">
+                <div className="card-body">
+                    <h2 className="text-3xl font-bold">{data[selectedSKill].description}</h2>
+                    </div>
+                </div>
+                </div>
+        </div>
 
     </>
 }
